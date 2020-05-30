7d416e8c716b43883245e91d5dd46baeef6457e9
Michael Glimmerdahl <michael.glimmerdahl@telekalkyl.se>
2020-05-30 10:01:30 +0200

    make it better

--- server.js
+++ server.js
@@ -12,9 +12,25 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'app', 'build')));
 
 // Connect to MongoDB
+/*
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
  .then(() => console.log("Database connected"))
  .catch(err => console.log(err));
+*/
+const db = process.env.MONGODB_URL;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    });
+    console.log("MongoDB is Connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
 
 // Specify routes
 const trainingsRouter = require("./routes/trainings");
