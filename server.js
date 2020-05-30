ff14875d385666f8e9e4639184b5c976c7c7f2be
Michael Glimmerdahl <michael.glimmerdahl@telekalkyl.se>
2020-05-30 11:59:14 +0200

    make it better

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'app', 'build')));
+app.use('/', express.static(path.join(__dirname, 'app/build')));
 
 // Connect to MongoDB
 const db = "mongodb+srv://dt162g_admin:McPfMcPf@cluster0-p2zig.mongodb.net/training?retryWrites=true&w=majority"
@@ -25,7 +25,7 @@ app.use("/trainings", trainingsRouter);
 app.use("/users", userRouter);
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'app', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'app/build/index.html'));
 });
 
 // Start local server
